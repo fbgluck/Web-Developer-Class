9afeda797c620d60ef1001dee490653fd483a3a4
Fredric Gluck <fbgluck@Freds-Imac.local>
2019-07-01 16:06:51 -0400

    more exercise

--- localGlobal.js
+++ localGlobal.js
@@ -2,6 +2,8 @@
 // Declaring Variables: 
 // var -- not recommended. Makes variable totally global
 // let -- recommended for all variables. Makes variable applicable in scope it was declared
+// if you don't declare (but just use a variable), it is by default global
+// ALWAYS declare variables before use
 let iAmGlobal = 'Global Value';
 if (true) {
     let iAmLocal = 'Local Value';
